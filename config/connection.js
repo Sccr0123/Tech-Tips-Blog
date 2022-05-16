c2f8842a7f48e712d1ce8d649f123a78545ce31e
Zachary Thomas <sccr0123@gmail.com>
2022-05-16 00:04:22 -0400

    Changed Setting In connection.js

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ const sequelize = process.env.JAWSDB_URL
 			process.env.DB_USER,
 			process.env.DB_PW,
 			{
-				host: "127.0.0.1" || "localhost",
+				host: "db" || "localhost",
 				dialect: "mysql",
 				port: 3306,
 			}
