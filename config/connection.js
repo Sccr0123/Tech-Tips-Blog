2f0c9ea6c8d57c98153d7b784d3764e0b4fa60a5
Zachary Thomas <sccr0123@gmail.com>
2022-05-16 18:14:20 -0400

    Corrected Heroku DB Issue

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 // create connection to our db
-const sequelize = process.env.JAWSDB_URL
+let sequelize = process.env.JAWSDB_URL
 	? new Sequelize(process.env.JAWSDB_URL)
 	: new Sequelize(
 			process.env.DB_NAME,
@@ -13,7 +13,7 @@ const sequelize = process.env.JAWSDB_URL
 				host: "localhost",
 				dialect: "mysql",
 				port: 3306,
-		}
-	);
+			}
+	  );
 
 module.exports = sequelize;
